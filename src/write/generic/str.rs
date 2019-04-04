6ae06ef5a7ccabfe2b77e7b4b767c1484bbcbf45
Денис Котляров <denis2005991@gmail.com>
2019-04-04 14:26:43 +0300

    1. Add alfa cluWrite.

--- src/write/generic/str.rs
+++ src/write/generic/str.rs
@@ -2,11 +2,14 @@
 use std::fmt;
 use std::io;
 
-pub trait WriteStr<'a, Ok, Err> {
-	fn write_str(&mut self, s: &'a str) -> Result<Ok, Err>;
+pub trait WriteStr<'a> {
+	type Ok;
+	type Err;
+	
+	fn write_str(&mut self, s: &'a str) -> Result<Self::Ok, Self::Err>;
 	
 	#[inline(always)]
-	fn write_str_array<'l>(&mut self, arr: &'l [&'a str]) -> Result<(), Err> {
+	fn write_str_array<'l>(&mut self, arr: &'l [&'a str]) -> Result<(), Self::Err> {
 		for str in arr {
 			let _e = self.write_str(str)?;
 		}
@@ -14,7 +17,7 @@ pub trait WriteStr<'a, Ok, Err> {
 	}
 	
 	#[inline(always)]
-	fn write_str_lenarray<'l>(&mut self, _all_size: usize, arr: &'l [&'a str]) -> Result<(), Err> {
+	fn write_str_lenarray<'l>(&mut self, _all_size: usize, arr: &'l [&'a str]) -> Result<(), Self::Err> {
 		self.write_str_array(arr)
 	}
 }
@@ -26,51 +29,60 @@ pub trait WriteStr<'a, Ok, Err> {
 	}
 }*/
 
-impl<'a, 's, T, Ok, Err> WriteStr<'a, Ok, Err> for &'s mut T where T: WriteStr<'a, Ok, Err> {
+impl<'a, 's, T, OK, ERR> WriteStr<'a> for &'s mut T where T: WriteStr<'a, Ok=OK, Err=ERR> {
+	type Ok = T::Ok;
+	type Err = T::Err;
+	
 	#[inline(always)]
-	fn write_str(&mut self, s: &'a str) -> Result<Ok, Err> {
+	fn write_str(&mut self, s: &'a str) -> Result<Self::Ok, Self::Err> {
 		T::write_str(self, s)
 	}
 	
 	#[inline(always)]
-	fn write_str_array<'l>(&mut self, arr: &'l [&'a str]) -> Result<(), Err> {
+	fn write_str_array<'l>(&mut self, arr: &'l [&'a str]) -> Result<(), Self::Err> {
 		T::write_str_array(self, arr)
 	}
 	
 	#[inline(always)]
-	fn write_str_lenarray<'l>(&mut self, _all_size: usize, arr: &'l [&'a str]) -> Result<(), Err> {
+	fn write_str_lenarray<'l>(&mut self, _all_size: usize, arr: &'l [&'a str]) -> Result<(), Self::Err> {
 		T::write_str_lenarray(self, _all_size, arr)
 	}
 }
 
-impl<'a, 's, Ok, Err> WriteStr<'a, Ok, Err> for &'s mut dyn WriteStr<'a, Ok, Err> {
+impl<'a, 's, OK, ERR> WriteStr<'a> for &'s mut dyn WriteStr<'a, Ok=OK, Err=ERR> {
+	type Ok = OK;
+	type Err = ERR;
+	
 	#[inline(always)]
-	fn write_str(&mut self, s: &'a str) -> Result<Ok, Err> {
+	fn write_str(&mut self, s: &'a str) -> Result<Self::Ok, Self::Err> {
 		WriteStr::write_str(*self, s)
 	}
 	
 	#[inline(always)]
-	fn write_str_array<'l>(&mut self, arr: &'l [&'a str]) -> Result<(), Err> {
+	fn write_str_array<'l>(&mut self, arr: &'l [&'a str]) -> Result<(), Self::Err> {
 		WriteStr::write_str_array(*self, arr)
 	}
 	
 	#[inline(always)]
-	fn write_str_lenarray<'l>(&mut self, _all_size: usize, arr: &'l [&'a str]) -> Result<(), Err> {
+	fn write_str_lenarray<'l>(&mut self, _all_size: usize, arr: &'l [&'a str]) -> Result<(), Self::Err> {
 		WriteStr::write_str_lenarray(*self, _all_size, arr)
 	}
 }
 
 
 
-impl<'a, Err> WriteStr<'a, (), Err> for Vec<u8> {
+impl<'a> WriteStr<'a> for Vec<u8> {
+	type Ok = ();
+	type Err = ();
+	
 	#[inline(always)]
-	fn write_str(&mut self, s: &'a str) -> Result<(), Err> {
+	fn write_str(&mut self, s: &'a str) -> Result<(), ()> {
 		self.extend_from_slice(s.as_bytes());
 		Ok( () )
 	}
 	
 	#[inline(always)]
-	fn write_str_array<'l>(&mut self, arr: &'l [&'a str]) -> Result<(), Err> {
+	fn write_str_array<'l>(&mut self, arr: &'l [&'a str]) -> Result<(), ()> {
 		for a in arr.into_iter() {
 			self.extend_from_slice(a.as_bytes());
 		}
@@ -79,7 +91,7 @@ impl<'a, Err> WriteStr<'a, (), Err> for Vec<u8> {
 	}
 	
 	#[inline(always)]
-	fn write_str_lenarray<'l>(&mut self, all_size: usize, arr: &'l [&'a str]) -> Result<(), Err> {
+	fn write_str_lenarray<'l>(&mut self, all_size: usize, arr: &'l [&'a str]) -> Result<(), ()> {
 		self.reserve(all_size);
 		for a in arr.into_iter() {
 			self.extend_from_slice(a.as_bytes());
@@ -88,16 +100,19 @@ impl<'a, Err> WriteStr<'a, (), Err> for Vec<u8> {
 	}
 }
 
-impl<'a, Err> WriteStr<'a, (), Err> for Vec<&'a str> {
+impl<'a> WriteStr<'a> for Vec<&'a str> {
+	type Ok = ();
+	type Err = ();
+	
 	#[inline(always)]
-	fn write_str(&mut self, s: &'a str) -> Result<(), Err> {
+	fn write_str(&mut self, s: &'a str) -> Result<(), ()> {
 		self.push(s);
 		
 		Ok( () )
 	}
 	
 	#[inline(always)]
-	fn write_str_array<'l>(&mut self, arr: &'l [&'a str]) -> Result<(), Err> {
+	fn write_str_array<'l>(&mut self, arr: &'l [&'a str]) -> Result<(), ()> {
 		for a in arr.into_iter() {
 			self.push(a);
 		}
@@ -106,7 +121,7 @@ impl<'a, Err> WriteStr<'a, (), Err> for Vec<&'a str> {
 	}
 	
 	#[inline(always)]
-	fn write_str_lenarray<'l>(&mut self, all_size: usize, arr: &'l [&'a str]) -> Result<(), Err> {
+	fn write_str_lenarray<'l>(&mut self, all_size: usize, arr: &'l [&'a str]) -> Result<(), ()> {
 		self.reserve(all_size);
 		for a in arr.into_iter() {
 			self.push(a);
@@ -118,16 +133,19 @@ impl<'a, Err> WriteStr<'a, (), Err> for Vec<&'a str> {
 
 
 
-impl<'a, Err> WriteStr<'a, (), Err> for String {
+impl<'a> WriteStr<'a> for String {
+	type Ok = ();
+	type Err = ();
+	
 	#[inline(always)]
-	fn write_str(&mut self, s: &'a str) -> Result<(), Err> {
+	fn write_str(&mut self, s: &'a str) -> Result<(), ()> {
 		self.push_str(s);
 		
 		Ok( () )
 	}
 	
 	#[inline(always)]
-	fn write_str_array<'l>(&mut self, arr: &'l [&'a str]) -> Result<(), Err> {
+	fn write_str_array<'l>(&mut self, arr: &'l [&'a str]) -> Result<(), ()> {
 		for a in arr.into_iter() {
 			self.push_str(a);
 		}
@@ -136,7 +154,7 @@ impl<'a, Err> WriteStr<'a, (), Err> for String {
 	}
 	
 	#[inline(always)]
-	fn write_str_lenarray<'l>(&mut self, all_size: usize, arr: &'l [&str]) -> Result<(), Err> {
+	fn write_str_lenarray<'l>(&mut self, all_size: usize, arr: &'l [&str]) -> Result<(), ()> {
 		self.reserve(all_size);
 		for a in arr.into_iter() {
 			self.push_str(a);
@@ -149,13 +167,19 @@ impl<'a, Err> WriteStr<'a, (), Err> for String {
 
 
 
-impl<'a, 's> WriteStr<'a, (), fmt::Error> for dyn fmt::Write + 's {
+impl<'a, 's> WriteStr<'a> for dyn fmt::Write + 's {
+	type Ok = ();
+	type Err = fmt::Error;
+	
 	#[inline(always)]
 	fn write_str(&mut self, s: &'a str) -> Result<(), fmt::Error> {
 		fmt::Write::write_str(self, s)
 	}
 }
-impl<'a, 's> WriteStr<'a, usize, io::Error> for dyn io::Write + 's {
+impl<'a, 's> WriteStr<'a> for dyn io::Write + 's {
+	type Ok = usize;
+	type Err = io::Error;
+	
 	#[inline(always)]
 	fn write_str(&mut self, s: &'a str) -> Result<usize, io::Error> {
 		io::Write::write(self, s.as_bytes())
@@ -163,56 +187,3 @@ impl<'a, 's> WriteStr<'a, usize, io::Error> for dyn io::Write + 's {
 }
 
 
-
-impl<'a, 's> WriteStr<'a, (), ErrWriteStr> for dyn fmt::Write + 's {
-	#[inline(always)]
-	fn write_str(&mut self, s: &str) -> Result<(), ErrWriteStr> {
-		if let Err(e) = fmt::Write::write_str(self, s) {
-			return Err(e.into());	
-		}
-		
-		Ok( () )
-	}
-}
-
-impl<'a, 's> WriteStr<'a, (), ErrWriteStr> for dyn io::Write + 's {
-	#[inline(always)]
-	fn write_str(&mut self, s: &'a str) -> Result<(), ErrWriteStr> {
-		if let Err(e) = io::Write::write(self, s.as_bytes()) {
-			return Err(e.into());
-		}
-		
-		Ok( () )
-	}
-}
-
-
-
-#[derive(Debug)]
-pub enum ErrWriteStr {
-	IO(io::Error),
-	FMT(fmt::Error),
-	None,
-}
-
-
-impl From<()> for ErrWriteStr {
-	#[inline(always)]
-	fn from(_r: ()) -> Self {
-		ErrWriteStr::None
-	}
-}
-
-impl From<io::Error> for ErrWriteStr {
-	#[inline(always)]
-	fn from(r: io::Error) -> Self {
-		ErrWriteStr::IO(r)
-	}
-}
-
-impl From<fmt::Error> for ErrWriteStr {
-	#[inline(always)]
-	fn from(r: fmt::Error) -> Self {
-		ErrWriteStr::FMT(r)	
-	}
-}
